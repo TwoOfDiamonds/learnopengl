61318c1626866aeaa1b3c86a72b5ca4f55b3447b
Daniel Alexandru Radu <danyhk94@gmail.com>
2018-10-17 14:16:01 +0300

    moving camera

--- learnopengl/Camera.cpp
+++ learnopengl/Camera.cpp
@@ -1,14 +1,39 @@
 #include "Camera.h"
+#include <glm\gtc\matrix_transform.hpp>
 
 
-
-Camera::Camera(const glm::mat4 &projectionMatrix) :
-	mProjectionMatrix(1.0f)
+Camera::Camera(const glm::mat4 &projectionMatrix, const glm::mat4 &transformationMatrix) :
+	mFinalMatrix(1.0f),
+	mProjectionMatrix(projectionMatrix),
+	mTransformationMatrix(transformationMatrix)
 {
-	mProjectionMatrix = projectionMatrix;
+	ComputeFinalMatrix();
 }
 
 void Camera::Draw(unsigned int shaderId, const SceneNode &sceneRoot) const
 {
-	sceneRoot.Draw(shaderId, mProjectionMatrix);
+	sceneRoot.Draw(shaderId, mFinalMatrix);
+}
+
+void Camera::SetTransformationMatrix(const glm::mat4 &transformationMatrix)
+{
+	mTransformationMatrix = transformationMatrix;
+	ComputeFinalMatrix();
+}
+
+void Camera::Translate(const glm::vec3 & translationVector)
+{
+	mTransformationMatrix = glm::translate(mTransformationMatrix, translationVector);
+	ComputeFinalMatrix();
+}
+
+void Camera::Rotate(float angle, const glm::vec3& rotationVector)
+{
+	mTransformationMatrix = glm::rotate(mTransformationMatrix, angle, rotationVector);
+	ComputeFinalMatrix();
+}
+
+inline void Camera::ComputeFinalMatrix()
+{
+	mFinalMatrix = mProjectionMatrix * mTransformationMatrix;
 }
